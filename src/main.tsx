3072917adfa6f45c6ce712f5df11a1ccd8cb7cab
5c7zck82bt-prog <5c7zck82bt@privaterelay.appleid.com>
2026-09-23 06:14:35 -0400

    Load card readability interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AudioProvider } from './audio/AudioContext'
 import './battleVisualRefresh.css'
+import './cardReadability.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
