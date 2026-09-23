fb3c80e750685291db0a68bf0b5a16d8b27fe029
5c7zck82bt-prog <5c7zck82bt@privaterelay.appleid.com>
2026-09-22 21:04:16 -0400

    Load HOODMON battle visual refresh styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AudioProvider } from './audio/AudioContext'
+import './battleVisualRefresh.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
