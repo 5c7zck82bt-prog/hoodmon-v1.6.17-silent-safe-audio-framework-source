e23adb264bacce80eb0c22548816cbfa382b53b2
5c7zck82bt-prog <5c7zck82bt@privaterelay.appleid.com>
2026-09-23 07:49:42 -0400

    Enable final battle polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import './battleLayoutV2.css'
 import './playerFieldMirror.css'
 import './inlineTurnControls.css'
 import './inlineTurnControls'
+import './battlePolishV3.css'
+import './battlePolish'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
