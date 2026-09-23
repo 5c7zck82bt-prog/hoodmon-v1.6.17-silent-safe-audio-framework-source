6b68294e55d4afed8e46abbd1d09a63aaf95ebf1
5c7zck82bt-prog <5c7zck82bt@privaterelay.appleid.com>
2026-09-23 08:11:53 -0400

    Remove unstable battle info dock imports

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,6 @@ import './battleLayoutV2.css'
 import './playerFieldMirror.css'
 import './inlineTurnControls.css'
 import './inlineTurnControls'
-import './rightBattleInfoDock.css'
-import './rightBattleInfoDock'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
