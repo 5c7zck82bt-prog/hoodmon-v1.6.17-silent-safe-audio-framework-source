3ee9bffddd6468c6cd244255af193b352627a14d
5c7zck82bt-prog <5c7zck82bt@privaterelay.appleid.com>
2026-09-23 08:04:07 -0400

    Restore stable battle stylesheet imports

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import './battleLayoutV2.css'
 import './playerFieldMirror.css'
 import './inlineTurnControls.css'
 import './inlineTurnControls'
-import './bottomBattleHud.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
